8e6ef3ae51792387a6dbbdc0b86d61f76f9098a1
DH-Travels <162419829+DH-Travels@users.noreply.github.com>
2024-03-06 15:23:58 +0800

    Basic format for Travel blog

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("from script file");
+
+function navigateToAbout() {
+    window.location.href = "about.html";
+}
+
+function navigateToHome() {
+    window.location.href = "home.html";
+}
